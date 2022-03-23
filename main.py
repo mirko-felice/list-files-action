6c52daae422d6d1b1d3c0527543f0655640f2f46
Mirko Felice <mirko.felice@studio.unibo.it>
2022-03-23 10:47:13 +0100

    refactor: align with python guidelines

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 import sys
 
-def removeLastOccurrence(string: str, char: str):
+
+def remove_last_occurrence(string: str, char: str):
     length = len(string)
     string2 = ''
     for i in range(length):
         if(string[i] == char):
             string2 = string[0:i] + string[i + 1:length]
     return string2
+
 
 def main():
     path = os.environ["INPUT_PATH"]
@@ -19,12 +21,13 @@ def main():
             if file.endswith(f'{extension}'):
                 paths = paths + '\"' + root + '/' + str(file) + '\", '
 
-    paths = removeLastOccurrence(paths, ',')
+    paths = remove_last_occurrence(paths, ',')
     paths = "[" + paths + "]"
     sys.stdout.write(f'::set-output name=paths::{paths}\n')
     print(paths)
 
     sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
